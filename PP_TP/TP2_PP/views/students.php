ace165354bba60b50adae2684de537ba4bdf4c82
Mathura <mathura.santhalingam@gmail.com>
2026-03-05 01:49:01 +0100

    TP2 amelioration rendu final -css

--- PP_TP/TP2_PP/views/students.php
+++ PP_TP/TP2_PP/views/students.php
@@ -2,20 +2,23 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des étudiants</title>
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
-<h1>Liste des étudiants</h1>
-
-<ul>
+<main>
+    <h1>Liste des étudiants</h1>
+    <ul class="student-list"> 
     <?php foreach ($students as $student): ?>
-        <li>
-            <?= $student['name'] ?> - <?= $student['age'] ?> ans
+        <li class="student-item"> 
+            <span class="name"><?= htmlspecialchars($student['name']) ?></span>
+            <span class="age"><?= htmlspecialchars($student['age']) ?> ans</span>
         </li>
     <?php endforeach; ?>
 </ul>
+</main>
 
 </body>
 </html>
